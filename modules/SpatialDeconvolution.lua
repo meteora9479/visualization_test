23f91ae90008df6687093b599fae8cb9cafdd951
meteora9479 <meteora9479@gmail.com>
2015-10-06 20:50:10 +0800

    modified SpatialDeconvolution module

--- modules/SpatialDeconvolution.lua
+++ modules/SpatialDeconvolution.lua
@@ -2,16 +2,14 @@ require'cudnn'
 local SpatialDeconvolution, parent = torch.class('cudnn.SpatialDeconvolution', 'cudnn.SpatialConvolution')
 
 
-function SpatialDeconvolution:__init( convLayer, reconstruction_size, firstDeconv )
+function SpatialDeconvolution:__init( convLayer, reconstruction_size, neuron_num, normal_deconv )
     assert( torch.typename(convLayer) == 'cudnn.SpatialConvolution', 'Input should be cudnn.SpatialConvolution()')
-    local deconvInputPlane = 1
-    if firstDeconv == true then 
-        deconvInputPlane = convLayer.nOutputPlane
-    end
-    
-    parent.__init(self, deconvInputPlane, convLayer.nInputPlane, convLayer.kW, convLayer.kH, convLayer.dW, 
+    parent.__init(self, convLayer.nOutputPlane, convLayer.nInputPlane, convLayer.kW, convLayer.kH, convLayer.dW, 
                   convLayer.dH, convLayer.padW, convLayer.padH, convLayer.groups)
     self:reset() 
+    
+    self.normal_deconv = normal_deconv or false
+    self.neuron_num = neuron_num or 0
     self.reconstruction_size = reconstruction_size
     self.weight=convLayer.weight
     self.gradWeight=convLayer.gradWeight    
@@ -20,12 +18,24 @@ end
 
 
 function SpatialDeconvolution:updateOutput(input)
-    local deconv1_fm = torch.CudaTensor( self.nInputPlane, self.nOutputPlane, self.reconstruction_size, 
-                                         self.reconstruction_size):zero():cuda()
-    local deconv1 = cudnn.SpatialConvolution(1, 1, self.kW, self.kH, 1, 1, 
-                                             math.floor(self.kW/2), math.floor(self.kH/2), self.group):cuda()
+    --print(self.normalDeconv)
+    local deconv_output = self.nInputPlane
+    local total_deconv = self.nInputPlane
+    if self.neuron_num ~= 0 then
+        total_deconv = 1
+        deconv_output = 1
+    end    
     
-    local conv_scat_fm = torch.zeros(self.nInputPlane, self.reconstruction_size, self.reconstruction_size):cuda()
+    if normal_deconv == true then
+        deconv_output = 1
+    end
+        
+    local deconv_fm = torch.CudaTensor( deconv_output, self.nOutputPlane, self.reconstruction_size, 
+                                         self.reconstruction_size):zero():cuda()
+    local deconv = cudnn.SpatialConvolution(1, 1, self.kW, self.kH, 1, 1, 
+                                            math.floor(self.kW/2), math.floor(self.kH/2), self.group):cuda()
+    
+    local conv_scat_fm = torch.zeros(total_deconv, self.reconstruction_size, self.reconstruction_size):cuda()
     local stride_size = self.dH
     local padding_size = (self.reconstruction_size - (input:size(2) * stride_size))/2
     
@@ -44,7 +54,7 @@ function SpatialDeconvolution:updateOutput(input)
         end
 
         local total_size = (n*x+padding_size*2)*(n*x+padding_size*2)
-        for i=1,self.nInputPlane do
+        for i=1,total_deconv do
             local m=torch.zeros(n*x+padding_size*2,n*x+padding_size*2):cuda()
             m:view(total_size,1):scatter(1,idx,input[i]:view(n*n,1))
             conv_scat_fm[i] = m
@@ -52,24 +62,39 @@ function SpatialDeconvolution:updateOutput(input)
     end  
         
     -- Deconv
-    for i=1, self.nInputPlane do
-        for j=1, self.nOutputPlane do
-            local fm = conv_scat_fm[i]
+    if self.normal_deconv == false then
+        for i=1, total_deconv do
+            for j=1, self.nOutputPlane do
+                local weight_index = i
+                if self.neuron_num ~= 0 then
+                    weight_index = self.neuron_num
+                end
 
-            deconv1.weight = self.weight[{ {i}, {j}, {}, {} }]:transpose(3, 4):contiguous()
-            local deconv_result = deconv1:forward(fm:view(1, self.reconstruction_size, self.reconstruction_size)):cuda()
-
+                local fm = conv_scat_fm[i]
+                deconv.weight = self.weight[{ {weight_index}, {j}, {}, {} }]:transpose(3, 4):contiguous()
+                local deconv_result = deconv:forward(fm:view(1, self.reconstruction_size, self.reconstruction_size)):cuda()
+                -- BGR to RGB
+                if self.nOutputPlane==3 then
+                    deconv_fm[{ {i}, {3-(j-1)}, {}, {} }] = deconv_result
+                else
+                    deconv_fm[{ {i}, {j}, {}, {} }] = deconv_result
+                end            
+            end
+        end
+    else
+        for j=1, self.nOutputPlane do 
+            deconv.weight = self.weight[{ {}, {j}, {}, {} }]:transpose(3, 4):contiguous()
+            local deconv_result = deconv:forward(fm:view(1, self.reconstruction_size, self.reconstruction_size)):cuda()
             -- BGR to RGB
             if self.nOutputPlane==3 then
-                deconv1_fm[{ {i}, {3-(j-1)}, {}, {} }] = deconv_result
+                deconv_fm[{ {1}, {3-(j-1)}, {}, {} }] = deconv_result
             else
-                deconv1_fm[{ {i}, {j}, {}, {} }] = deconv_result
+                deconv_fm[{ {1}, {j}, {}, {} }] = deconv_result
             end
-            
         end
-    end
-
+    end   
+     
     cutorch.synchronize()    
-    return deconv1_fm
+    return deconv_fm
 end 
 
