311ddbec002f90a9087884923b74cc93044cf285
meteora9479 <meteora9479@gmail.com>
2015-11-16 14:46:11 +0800

    update module test

--- module_test.lua
+++ module_test.lua
@@ -2,6 +2,7 @@ require 'cudnn'
 require 'inn'
 require 'image'
 require 'dcnn'
+gfx = require 'gfx.js'
 
 -- Loads the mapping from net outputs to human readable labels
 function load_synset()
@@ -63,219 +64,74 @@ for i=1,5 do
 end
 
 
-
-
--------------------------------------------------------Testing_module---------------------------------------------------
-
-
-local SpatialDeconvolution, parent = torch.class('SpatialDeconvolution', 'cudnn.SpatialConvolution')
-
-function SpatialDeconvolution:__init( convLayer, reconstruction_size, neuron_num, normal_deconv )
-    print('module bug')
-    assert( torch.typename(convLayer) == 'cudnn.SpatialConvolution', 'Input should be cudnn.SpatialConvolution()')
-    parent.__init(self, convLayer.nOutputPlane, convLayer.nInputPlane, convLayer.kW, convLayer.kH, convLayer.dW, 
-                  convLayer.dH, convLayer.padW, convLayer.padH, convLayer.groups)
-    self:reset() 
-    
-    self.normal_deconv = normal_deconv or false
-    self.neuron_num = neuron_num or 0
-    if self.neuron_num == true then
-        self.neuron_num = 0 
-        self.normal_deconv = true
-    end
-    
-    if self.neuron_num == false then
-        self.neuron_num = 0 
-        self.normal_deconv = false
-    end 
-    
-    
-    self.reconstruction_size = reconstruction_size
-    self.weight=convLayer.weight:clone()
-        
-    flip = function(m,d) return m:index(d,torch.range(m:size(d),1,-1):long())end
-    self.weight = flip(flip(self.weight,4),3)
-    self.gradWeight=convLayer.gradWeight    
-    
-    print('deconv layer has been built !')
-end
-
-function SpatialDeconvolution:updateOutput(input)
-    local deconv_output = self.nInputPlane
-    local total_deconv = self.nInputPlane
-    if torch.type(self.neuron_num)~='torch.IntTensor' and self.neuron_num ~= 0 then
-        total_deconv = 1
-        deconv_output = 1
-    end    
-    
-    if torch.type(self.neuron_num) == 'torch.IntTensor' then
-        total_deconv = self.neuron_num:size(1)
-        deconv_output = 1
-    end
-    
-    if normal_deconv == true then
-        deconv_output = 1
-    end
-            
-    local deconv_fm = torch.CudaTensor( deconv_output, self.nOutputPlane, self.reconstruction_size, 
-                                         self.reconstruction_size):zero():cuda()
-    local deconv = cudnn.SpatialConvolution(1, 1, self.kW, self.kH, 1, 1, 
-                                            math.floor(self.kW/2), math.floor(self.kH/2)):cuda()
-    
-    local conv_scat_fm = torch.zeros(total_deconv, self.reconstruction_size, self.reconstruction_size):cuda()
-    local stride_size = self.dH
-    local padding_size = (self.reconstruction_size - (input:size(2) * stride_size))/2
-    
-    local n=input:size(2)
-    local x=stride_size
-    input:cuda()
-   
-    --timer = torch.Timer()
-    if self.reconstruction_size ~= input:size(2) then
-        -- Scatter     contributed by TingFan
-        local idx=torch.LongTensor(n*n,1):cuda()
-        local counter=1;
-        for i=x,n*x,x do
-            for j=x,n*x,x do
-                idx[counter]=(i+math.floor(padding_size)) *(n*x+padding_size*2) + j + math.floor(padding_size)
-                counter=counter+1;
-            end
-        end
-
-        local total_size = (n*x+padding_size*2)*(n*x+padding_size*2)
-        for i=1,total_deconv do
-            local fm_index = i
-            
-            if torch.type(self.neuron_num) ~= 'torch.IntTensor' then
-                if self.neuron_num ~= 0 then
-                     fm_index = self.neuron_num
-                end
-            else
-                fm_index = self.neuron_num[i]
-            end
-                
-            local m=torch.zeros(n*x+padding_size*2,n*x+padding_size*2):cuda()
-            local output = input[fm_index]:view(n*n,1)
-            m:view(total_size,1):scatter(1,idx,output)
-            conv_scat_fm[i] = m
-        end      
-    else
-        if torch.type(self.neuron_num) ~= 'torch.IntTensor' and self.neuron_num ~= 0 then
-            conv_scat_fm[{{1},{},{}}] = input[{{self.neuron_num},{},{}}]:clone()
-        elseif torch.type(self.nuuron_num) == 'torch.IntTensor' then
-            for i=1,self.nuuron_num:size(1) do
-                conv_scat_fm[i] = input[{{self.neuron_num[i]},{},{}}]:clone()
-            end
-        else
-            conv_scat_fm = input:clone()
-        end
-    end
-        
-    --print('==> Scatter Time elapsed: ' .. timer:time().real .. ' seconds')
-    --timer2 = torch.Timer()
-    -- Deconv
-    if self.normal_deconv == false then
-        if torch.type(self.neuron_num) ~= 'torch.IntTensor' then -- single_neuron
-            for i=1, total_deconv do
-                for j=1, self.nOutputPlane do
-                    local weight_index = i
-                    if self.neuron_num ~= 0 then
-                        weight_index = self.neuron_num
-                    end
-
-                    local fm = conv_scat_fm[i]
-                    deconv.weight = self.weight[weight_index][j]:clone()
-                    local deconv_result = deconv:forward(fm:view(1, self.reconstruction_size, self.reconstruction_size):cuda())
-                    -- BGR to RGB
-                    if self.nOutputPlane==3 then
-                        deconv_fm[{ {i}, {3-(j-1)}, {}, {} }] = deconv_result
-                    else
-                        deconv_fm[{ {i}, {j}, {}, {} }] = deconv_result
-                    end            
-                end
-            end      
-        else
-            for i=1, total_deconv do
-                for j=1, self.nOutputPlane do
-                    local weight_index = i
-                    weight_index = self.neuron_num[i]
-                    local fm = conv_scat_fm[i]
-                    deconv.weight = self.weight[weight_index][j]:clone()
-                    local deconv_result = deconv:forward(fm:view(1, self.reconstruction_size, self.reconstruction_size)):cuda()
-                    -- BGR to RGB
-                    if self.nOutputPlane==3 then
-                        deconv_fm[{ {1}, {3-(j-1)}, {}, {} }] = deconv_fm[{ {1}, {3-(j-1)}, {}, {} }] + deconv_result
-                    else
-                        deconv_fm[{ {1}, {j}, {}, {} }] = deconv_fm[{ {1}, {j}, {}, {} }] + deconv_result
-                    end                       
-                end
-            end
-        end
-    else
-        local deconv_normal = cudnn.SpatialConvolution( self.nInputPlane, self.nOutputPlane, self.kW, self.kH, 1, 1, 
-                                                        math.floor(self.kW/2), math.floor(self.kH/2), self.group):cuda()         
-        deconv_normal.weight = torch.CudaTensor( self.nOutputPlane, self.nInputPlane, self.kW, self.kH )
-        
-        timer2 = torch.Timer()
-        deconv_normal.weight = self.weight:transpose(1, 2):contiguous()          
-        deconv_fm = deconv_normal:forward(conv_scat_fm):cuda()
-        -- BGR to RGB
-        if self.nOutputPlane==3 then
-          local temp = deconv_fm:clone()
-          deconv_fm[{1,{},{}}] = temp[{3,{},{}}]
-          deconv_fm[{3,{},{}}] = temp[{1,{},{}}]   
-        end
-    end   
-    
-    --print('==> Deconv Time elapsed: ' .. timer2:time().real .. ' seconds')
-    cutorch.synchronize()
-    if deconv_fm:dim() == 4 and deconv_output==1 then
-        self.output = deconv_fm[1]
-        return deconv_fm[1]:clone() 
-    end    
-    
-    self.output = deconv_fm
-    return deconv_fm
-end 
-
--------------------------------------------------------Testing_module------------------------------------------------end
-
-
-
-
-
 -- set deconvNet
 local deconvNet = nn.Sequential()
 
 local conv5_fm = net:get(1):get(13).output
 
---print(net:get(1):get(11).output:size(2))
+require'testing_deconvLayer'
+
+-- deconvNet:add(Test_SpatialDeconvolution( net:get(1):get(13), net:get(1):get(11).output:size(2), 27 ))
+-- deconvNet:add(cudnn.ReLU(true))
+-- deconvNet:add(Test_SpatialDeconvolution( net:get(1):get(11), net:get(1):get(9).output:size(2), true ))
+-- deconvNet:add(cudnn.ReLU(true))
+-- deconvNet:add(Test_SpatialDeconvolution( net:get(1):get(9), net:get(1):get(8).output:size(2), true ))
+-- deconvNet:add(unpooling_layers[2])
+-- deconvNet:add(cudnn.ReLU(true))
+-- deconvNet:add(Test_SpatialDeconvolution( net:get(1):get(5), net:get(1):get(4).output:size(2), true ))
+-- deconvNet:add(unpooling_layers[1])
+-- deconvNet:add(cudnn.ReLU(true))
+-- deconvNet:add(Test_SpatialDeconvolution( net:get(1):get(1), I:size(2), true ))
+
+deconv1 = Test_SpatialDeconvolution( net:get(1):get(1), I:size(2), 27 )
+conv1 = net:get(1):get(1)
+
+-- vis_c1 = deconv1:forward( net:get(1):get(1).output )
+-- print(vis_c1:size() )
+-- gfx.image(vis_c1)
 
 
-deconvNet:add( SpatialDeconvolution( net:get(1):get(13), 13, 27 ) )
---deconvNet:add( SpatialDeconvolution( net:get(1):get(13), net:get(1):get(11).output:size(2), 27 ) )
-print('bug')
+-- error_tensor = 0
+-- last_output = 0
+-- local last_time = 0
+-- layer_n = 1
 
 
-deconvNet:add(cudnn.ReLU(true))
-deconvNet:add(SpatialDeconvolution( net:get(1):get(11), net:get(1):get(9).output:size(2), true ))
-deconvNet:add(cudnn.ReLU(true))
-deconvNet:add(SpatialDeconvolution( net:get(1):get(9), net:get(1):get(8).output:size(2), true ))
-deconvNet:add(unpooling_layers[2])
-deconvNet:add(cudnn.ReLU(true))
-deconvNet:add(SpatialDeconvolution( net:get(1):get(5), net:get(1):get(4).output:size(2), true ))
-deconvNet:add(unpooling_layers[1])
-deconvNet:add(cudnn.ReLU(true))
-deconvNet:add(SpatialDeconvolution( net:get(1):get(1), I:size(2), true ))
+for i=1,20 do
+    deconv_timer = torch.Timer()
+    --vis_c5 = deconvNet:forward(conv5_fm)
+    vis_c1 = deconv1:forward( net:get(1):get(1).output )
+    --conv1_fm = conv1:forward(I:cuda()) 
+    
+    print('==> Time elapsed: ' .. deconv_timer:time().real .. ' seconds')
+    
+    if i==1 then
+        last_output = vis_c1
+        -- last_output = conv1.output 
+        -- last_output = scat_fm
+        -- last_output = weight
+    end
+    
+    error_tensor = last_output - vis_c1
+    -- error_tensor = last_output - conv1.output
+    -- error_tensor = last_output - scat_fm
+    -- error_tensor = last_output - weight
+    local test_error = 0
+    if type(error_tensor) ~= 'number' then 
+        for j=1, error_tensor:view(-1):size(1) do
+            test_error = test_error + error_tensor:view(-1)[j]
+        end    
+        
+        print( error_tensor:type() )
+    else
+        test_error = error_tensor
+        print( type(error_tensor) )
+    end
+    
+    print(test_error)
+    --gfx.image(image.toDisplayTensor( {input=net:get(1):get(13).output, padding=2, nrow=16} ) )
+    --gfx.image(vis_c5)
+    --gfx.image(vis_c1)
+end
 
 
-
-
-deconv_timer = torch.Timer()
-vis_c5 = deconvNet:forward(conv5_fm)
-
-print('==> Time elapsed: ' .. deconv_timer:time().real .. ' seconds')
-
-gfx = require 'gfx.js'
-
-gfx.image(vis_c5)
---
+++ testing_deconvLayer.lua
@@ -0,0 +1,184 @@
+
+local Test_SpatialDeconvolution, parent = torch.class('Test_SpatialDeconvolution', 'cudnn.SpatialConvolution')
+
+function Test_SpatialDeconvolution:__init( convLayer, reconstruction_size, neuron_num )
+    assert( torch.typename(convLayer) == 'cudnn.SpatialConvolution', 'Input should be cudnn.SpatialConvolution()')
+    parent.__init(self, convLayer.nOutputPlane, convLayer.nInputPlane, convLayer.kW, convLayer.kH, convLayer.dW, 
+                  convLayer.dH, convLayer.padW, convLayer.padH, convLayer.groups)
+    self:reset() 
+    
+    self.normal_deconv = normal_deconv or false
+    self.neuron_num = neuron_num or 0
+    if self.neuron_num == true then
+        self.neuron_num = 0 
+        self.normal_deconv = true
+    end
+    
+    if self.neuron_num == false then
+        self.neuron_num = 0 
+        self.normal_deconv = false
+    end 
+    
+    
+    self.reconstruction_size = reconstruction_size
+    self.weight=convLayer.weight:clone()
+        
+    flip = function(m,d) return m:index(d,torch.range(m:size(d),1,-1):long())end
+    self.weight = flip(flip(self.weight,4),3)
+    self.gradWeight=convLayer.gradWeight    
+    
+    self.deconv = {}
+    for i=1, convLayer.nOutputPlane do
+        self.deconv[i] = cudnn.SpatialConvolution(1, convLayer.nInputPlane, self.kW, self.kH, 1, 1, 
+                                             math.floor(self.kW/2), math.floor(self.kH/2) ):cuda()
+
+        self.deconv[i].weight = self.weight[{{i}, {}, {}, {} }]:transpose(1,2):contiguous():cuda()
+    end
+
+    self.deconv_normal = cudnn.SpatialConvolution( self.nInputPlane, self.nOutputPlane, self.kW, self.kH, 1, 1, 
+                                                        math.floor(self.kW/2), math.floor(self.kH/2)):cuda()         
+    self.deconv_normal.weight = self.weight:transpose(1, 2):contiguous():cuda()    
+        
+    print( convLayer )
+    print('deconv layer has been built !')
+end
+
+function Test_SpatialDeconvolution:updateOutput(input)
+    local deconv_output = self.nInputPlane
+    local total_deconv = self.nInputPlane
+    if torch.type(self.neuron_num)~='torch.IntTensor' and self.neuron_num ~= 0 then
+        total_deconv = 1
+        deconv_output = 1
+    end    
+    
+    if torch.type(self.neuron_num) == 'torch.IntTensor' then
+        total_deconv = self.neuron_num:size(1)
+        deconv_output = 1
+    end
+    
+    if normal_deconv == true then
+        deconv_output = 1
+    end
+            
+    local deconv_fm = torch.CudaTensor( deconv_output, self.nOutputPlane, self.reconstruction_size, 
+                                         self.reconstruction_size):cuda():zero()
+    
+    local conv_scat_fm = torch.zeros(total_deconv, self.reconstruction_size, self.reconstruction_size):cuda()
+    local stride_size = self.dH
+    local padding_size = (self.reconstruction_size - (input:size(2) * stride_size))/2
+    local increased_size = self.reconstruction_size - (input:size(2) * stride_size)
+    
+    local n=input:size(2)
+    local x=stride_size
+    input:cuda()
+   
+    --timer = torch.Timer()
+    if self.reconstruction_size ~= input:size(2) then
+        -- Scatter     contributed by TingFan
+        local idx=torch.LongTensor(n*n,1):cuda()
+        local counter=1;
+        for i=x,n*x,x do
+            for j=x,n*x,x do
+                idx[counter]=(i+math.floor(padding_size)) *(n * x + increased_size) + j + math.floor(padding_size)
+                counter=counter+1;
+            end
+        end
+
+        local total_size = (n*x+increased_size)*(n*x+increased_size)
+        for i=1,total_deconv do
+            local fm_index = i
+            
+            if torch.type(self.neuron_num) ~= 'torch.IntTensor' then
+                if self.neuron_num ~= 0 then
+                     fm_index = self.neuron_num
+                end
+            else
+                fm_index = self.neuron_num[i]
+            end
+                
+            local m=torch.zeros(n*x+increased_size,n*x+increased_size):cuda()
+            local output = input[fm_index]:view(n*n,1)
+            m:view(total_size,1):scatter(1,idx,output)
+            conv_scat_fm[i] = m
+        end      
+    else
+        if torch.type(self.neuron_num) ~= 'torch.IntTensor' and self.neuron_num ~= 0 then
+            conv_scat_fm[{{1},{},{}}] = input[{{self.neuron_num},{},{}}]:clone()
+        elseif torch.type(self.nuuron_num) == 'torch.IntTensor' then
+            for i=1,self.nuuron_num:size(1) do
+                conv_scat_fm[i] = input[{{self.neuron_num[i]},{},{}}]
+            end
+        else
+            conv_scat_fm = input
+        end
+    end
+        
+    --print('==> Scatter Time elapsed: ' .. timer:time().real .. ' seconds')
+    --timer2 = torch.Timer()
+    local test_return = nil
+    
+    -- Deconv
+    if self.normal_deconv == false then
+        if torch.type(self.neuron_num) ~= 'torch.IntTensor' then -- single_neuron
+            for i=1, total_deconv do
+                local weight_index = i
+                if self.neuron_num ~= 0 then
+                    weight_index = self.neuron_num
+                end
+
+                local fm = conv_scat_fm[i]                    
+                local deconv_result = self.deconv[weight_index]:forward(fm:view(1, self.reconstruction_size, 
+                                                                        self.reconstruction_size):cuda())
+                deconv_fm = deconv_result:contiguous():cuda()
+--                 --test deconv_result
+--                 if j==3 then
+--                     -- test_return = deconv.weight[1][1]:clone()
+--                     test_return = deconv_result:clone()
+--                 end
+
+--                 local error_tensor = deconv_result - deconv_fm
+--                 local test_error = 0
+--                 for j=1, error_tensor:view(-1):size(1) do
+--                     test_error = test_error + error_tensor:view(-1)[j]
+--                 end
+
+--                 print( 'error_fm:  ' .. test_error )
+                                        
+                --print( 'single_neuron' )
+
+            end      
+        else
+            for i=1, total_deconv do 
+                local weight_index = self.neuron_num[i]
+                local fm = conv_scat_fm[i]
+                local deconv_result = self.deconv[weight_index]:forward(fm:view(1, self.reconstruction_size,
+                                                                        self.reconstruction_size)):cuda()
+                deconv_fm = deconv_fm + deconv_result                     
+            end
+            
+            --print( 'multi_neuron' )
+        end
+    else                  
+        deconv_fm = self.deconv_normal:forward(conv_scat_fm):cuda()        
+        --print( 'normal_deconv' )
+    end   
+    
+    --print('==> Deconv Time elapsed: ' .. timer2:time().real .. ' seconds')
+    
+    -- BGR to RGB
+    if self.nOutputPlane==3 then
+      local temp = deconv_fm:clone()
+      deconv_fm[{1,{},{}}] = temp[{3,{},{}}]
+      deconv_fm[{3,{},{}}] = temp[{1,{},{}}]   
+    end    
+    
+    cutorch.synchronize()
+    if deconv_fm:dim() == 4 and deconv_output==1 then
+        -- print('dim == 4' )
+        self.output = deconv_fm[1]
+        return deconv_fm[1]
+    end    
+        
+    self.output = deconv_fm
+    return deconv_fm
+end 
